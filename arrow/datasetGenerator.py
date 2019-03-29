ee86a5ecd4385cd162e59e43ac88a6cc06297964
lucianogg <lucgarc97@hotmail.com>
2019-03-29 22:42:08 +0100

    Anade lo necesario para hacer lo nuestro

---
+++ arrow/datasetGenerator.py
@@ -0,0 +1,62 @@
+# Autores:
+# Luciano Garcia Giordano - 150245
+# Gonzalo Florez Arias - 150048
+# Salvador Gonzalez Gerpe - 150044
+
+import numpy as np
+import cv2
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+import config
+
+# hsImages = np.memmap('./datasets/hsImages' + config.datasetName + '.driver', dtype='uint8', mode='r', shape=(config.numberOfImages, config.imageShape['height'], config.imageShape['width'], 2))
+# markedImages = np.memmap('./datasets/markedImages' + config.datasetName + '.driver', dtype='uint8', mode='r', shape=(config.numberOfImages, config.imageShape['height'], config.imageShape['width'], 3))
+
+# hsVector = hsImages.reshape((config.numberOfImages*config.imageShape['height']*config.imageShape['width'],2))
+# markedVector = markedImages.reshape((config.numberOfImages*config.imageShape['height']*config.imageShape['width'],3))
+
+# hsExpanded = np.zeros((config.numberOfImages*config.imageShape['height']*config.imageShape['width'],3), dtype='uint8')
+# hsExpanded[:,:-1] = hsVector
+
+# for i in range(hsExpanded.shape[0]):
+#     pixel = markedVector[i]
+#     if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 255: # linea
+#         pixelClass = 0
+#     elif pixel[0] == 0 and pixel[1] == 255 and pixel[2] == 0: # suelo
+#         pixelClass = 1
+#     elif pixel[0] == 255 and pixel[1] == 0 and pixel[2] == 0: # marca
+#         pixelClass = 2
+#     else:
+#         pixelClass = 3
+        
+#     hsExpanded[i,2] = pixelClass
+
+# shapeD = hsExpanded[hsExpanded[:,2] != 3].shape
+# print shapeD
+# dataset = np.memmap('./datasets/dataset' + config.datasetName + '.driver', dtype=np.uint8, mode='w+', shape=shapeD)
+
+# dataset[:] = hsExpanded[hsExpanded[:,2] != 3]
+# dataset.flush()
+
+
+shapeD  = (20663, 3)
+dataset = np.memmap('./datasets/dataset' + config.datasetName + '.driver', dtype=np.uint8, mode='r', shape=shapeD)
+
+
+
+
+
+
+
+
+# shapeD = (61983, 3) # good for Alpera
+# dataset = np.memmap('./datasets/datasetManzana.driver', dtype=np.uint8, mode='r', shape=shapeD)
+
+
+
+
+
+# imagenes con pixeles en RGB
+# |H|S|Clase|
+
+
+
